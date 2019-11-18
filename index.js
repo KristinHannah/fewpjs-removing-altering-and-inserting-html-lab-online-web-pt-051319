37639c7a35de077a0d8e325ff6926b3da7d90389
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-18 02:09:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let el = document.getElementById("main");
 el.remove();
 
-var newHeader = document.createElement('h1#victory'); 
+let newHeader = document.createElement('h1#victory'); 
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "YOUR-NAME is the champion";
 
